954e432472c2e3bdb7c319346518e2ba343b34d4
Jade Abel <jade.marie.abel@gmail.com>
2021-07-22 22:03:56 -0400

    copied landing page card

--- alldata.js
+++ alldata.js
@@ -1,9 +1,27 @@
-function AllData() {
+/*function AllData() {
   const ctx = React.useContext(UserContext);
+
   return (
     <h1>
       All Data <br />
       {JSON.stringify(ctx)}
     </h1>
+  );
+}
+*/
+function AllData() {
+  return (
+    <div className="d-flex p-4">
+      <Card
+        bgcolor="success"
+        txtcolor="white"
+        header="BAD BANK LANDING PAGE"
+        title="WELCOME TO THE BANK"
+        text="'Here, you can pretend to be a trillionaire.'"
+        body={
+          <img src="bank.png" className="img-fluid" alt="Responsive image" />
+        }
+      />
+    </div>
   );
 }
