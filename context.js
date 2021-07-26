9fbfabeedc4dfce6d4898a2419b4f92f967fb613
Jade Abel <jade.marie.abel@gmail.com>
2021-07-25 20:25:07 -0400

    added dataRow and removed console log

--- context.js
+++ context.js
@@ -5,14 +5,13 @@ const UserContext = React.createContext(null);
 
 const Card = (props) => {
   const classes = () => {
-    console.log("bg test", props.bgcolor);
     const bg = props.bgcolor ? "bg-" + props.bgcolor : "success";
     const txt = props.txtcolor ? "text-" + props.txtcolor : "text-white";
     return "card mb-3" + " " + bg + " " + txt;
   };
 
   return (
-    <div className={classes()} style={{ maxWidth: "18rem" }}>
+    <div className={classes()} style={{ maxWidth: "fluid" }}>
       <div className="card-header">{props.header}</div>
       <div className="card-body">
         {props.title && <h5 className="card-title">{props.title}</h5>}
@@ -21,6 +20,16 @@ const Card = (props) => {
         {props.status && <div id="createStatus">{props.status}</div>}
       </div>
     </div>
+  );
+};
+
+const DataRow = (props) => {
+  return (
+    <ul className="list-group list-group-horizontal">
+      <li className="list-group-item"> {props.name} </li>
+      <li className="list-group-item"> {props.email} </li>
+      <li className="list-group-item"> {props.password}</li>
+    </ul>
   );
 };
 //these components are from the ReactRouter library that we loaded on our HTML
